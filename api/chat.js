f178b40be85528ce59d44a96b062dd30dc671ae0
xinixaxiom <xinixaxiom@gmail.com>
2026-05-27 15:58:05 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
 
           'Content-Type': 'application/json',
 
-          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+          'Authorization': 'Bearer ' + process.env.OPENAI_API_KEY
 
         },
 
